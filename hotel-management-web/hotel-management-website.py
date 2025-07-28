ddac7d6cdcdabc9cc6f1b9234f07fac45edb2f7d
Avryl-5 <daia2@papanui.school.nz>
2025-07-28 12:29:02 +1200

    Update hotel-management-website.py

--- hotel-management-web/hotel-management-website.py
+++ hotel-management-web/hotel-management-website.py
@@ -3,42 +3,41 @@ db = sqlite3.connect('hotel_management.db')
 
 
 #get everything about everyroom
-#Get_all_guests = db.cursor()
-#allguestsinfo = Get_all_guests.execute('SELECT * FROM GUESTS').fetchall()
-
+Get_all_guests = db.cursor()
+allguestsinfo = Get_all_guests.execute('SELECT * FROM GUESTS').fetchall()
 #for a in allguestsinfo:
 #    print(a)
 
 
 #get everything about everyroom
-#get_all_rooms = db.cursor()
-#roomsinfo = get_all_rooms.execute('SELECT * FROM ROOMS').fetchall()
+get_all_rooms = db.cursor()
+roomsinfo = get_all_rooms.execute('SELECT * FROM ROOMS').fetchall()
 #for b in roomsinfo:
 #    print(b)
 
 #bridging ables
-#get_guestreservations = db.cursor()
-#GRrelationship = get_guestreservations.execute('SELECT * FROM GUESTS_RESERVATIONS').fetchall()
+get_guestreservations = db.cursor()
+GRrelationship = get_guestreservations.execute('SELECT * FROM GUESTS_RESERVATIONS').fetchall()
 #for c in GRrelationship:
 #    print(c)
 
 
-#get_roomreservations = db.cursor()
-#RRrelationship = get_roomreservations.execute('SELECT * FROM ROOMS_RESERVATIONS').fetchall()
+get_roomreservations = db.cursor()
+RRrelationship = get_roomreservations.execute('SELECT * FROM ROOMS_RESERVATIONS').fetchall()
 #for d in RRrelationship:
 #    print(d)
 
 #statuses
-#get_statuses = db.cursor()
-#statuesinfo = get_statuses.execute('SELECT * FROM STATUS').fetchall()
+get_statuses = db.cursor()
+statuesinfo = get_statuses.execute('SELECT * FROM STATUS').fetchall()
 #for e in statuesinfo:
 #    print(e)
 
 
-#get_HKPingstatuses = db.cursor()
-#HKPingstatuesinfo = get_HKPingstatuses.execute('SELECT * FROM HOUSEKEEPINGS').fetchall()
+get_HKPingstatuses = db.cursor()
+HKPingstatuesinfo = get_HKPingstatuses.execute('SELECT * FROM HOUSEKEEPINGS').fetchall()
 #for f in HKPingstatuesinfo:
-#    print(f)
+    #print(f)
 
 
 #room types
