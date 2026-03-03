d853cf05dac9137f3457df28a2e6b6a35e699c38
Avryl-5 <daia2@papanui.school.nz>
2026-03-03 16:59:51 +1300

    Delete hm_program_complete directory

--- hm_program_complete/test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-db = sqlite3.connect("hotel_management.db")
-cursor = db.cursor()
-sql = "SELECT * FROM GUESTS;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-
-db.close()
